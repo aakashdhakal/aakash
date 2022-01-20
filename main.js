1ba37223b1e04854194a6b38d06c4009f2458221
A.D Entertainment <53038685+aakashdhakal@users.noreply.github.com>
2022-01-20 15:05:46 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b84d82e4133ae8496630be24215138d7ddff3ec5';
+window.smartsupp || (function (d) {
+  var s, c, o = smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+
+
+ function toggleTheme() {
+            var theme = document.getElementsByTagName('link')[0];
+            if (theme.getAttribute('href') == 'style.css') {
+                theme.setAttribute('href', 'dark.css');
+            } else {
+                theme.setAttribute('href', 'style.css');
+            }
+        }
+		
