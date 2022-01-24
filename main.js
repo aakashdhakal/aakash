30b835fef4af87a8f3e61a99c8082e5cfa05021d
A.D Entertainment <53038685+aakashdhakal@users.noreply.github.com>
2022-01-24 22:40:27 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -7,23 +7,5 @@ window.smartsupp || (function (d) {
   c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
 })(document);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeStylesheet = document.getElementById('theme');
-    const storedTheme = localStorage.getItem('theme');
-    if(storedTheme){
-        themeStylesheet.href = storedTheme;
-    }
-    const themeToggle = document.getElementById('theme-toggle');
-    themeToggle.addEventListener('click', () => {
-        // if it's light -> go dark
-        if(themeStylesheet.href.includes('style')){
-            themeStylesheet.href = 'dark.css';
-        } else {
-            // if it's dark -> go light
-            themeStylesheet.href = 'style.css';
-        }
-        // save the preference to localStorage
-        localStorage.setItem('theme',themeStylesheet.href)
-    })
-})
+
 		
