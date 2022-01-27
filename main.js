b2d98a921116f7f01d7e0d9476bb9abd64a36a26
A.D Entertainment <53038685+aakashdhakal@users.noreply.github.com>
2022-01-27 22:22:10 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -19,16 +19,16 @@ let calcScrollValue = () => {
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
 
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/61f13b72b9e4e21181bbfdce/1fqb50f1i';
+s1.src='https://embed.tawk.to/61f2c971b9e4e21181bc3c1b/1fqe65pnm';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('nav-mode_switch').addEventListener('click', () => {
