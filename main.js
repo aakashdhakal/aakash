2d83cf7b0b1e4ef9fc60d89596b87affde046dbf
A.D Entertainment <53038685+aakashdhakal@users.noreply.github.com>
2022-01-30 21:59:50 +0545

    Add files via upload

--- main.js
+++ main.js
@@ -48,26 +48,3 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-      let testForm = document.querySelector("#contact-form form");
-      
-      testForm.addEventListener('submit', e => {
-        e.preventDefault();
-
-        const formData = new FormData(testForm);
-        fetch(testForm.getAttribute('action'), {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-          },
-          body: new URLSearchParams(formData).toString()
-        })
-        .then(res => {
-          if (res) {
-            M.toast({
-              html: 'Thank you for your submission!',
-              classes: 'pulse'
-            });
-          }
-        });
-      });
